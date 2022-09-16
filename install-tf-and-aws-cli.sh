14c36fe8ab3f8fc766db83e31671c25d70e497fd
Eric Castoldi <eric.castoldi@gmail.com>
2022-09-16 19:38:23 -0300

    Fix install tf and aws cli

--- install-tf-and-aws-cli.sh
+++ install-tf-and-aws-cli.sh
@@ -3,10 +3,10 @@
 # aws cli
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
-./aws/install
+sudo ./aws/install
 
 # terraform
 wget https://releases.hashicorp.com/terraform/"$tf_version"/terraform_"$tf_version"_linux_amd64.zip
 unzip terraform_"$tf_version"_linux_amd64.zip
-mv terraform /usr/local/bin/
+sudo mv terraform /usr/local/bin/
 rm terraform_"$tf_version"_linux_amd64.zip
