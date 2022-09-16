f7d480fb9def13a2a5d21189fca1ddef550dd152
Eric Castoldi <eric.castoldi@gmail.com>
2022-09-16 19:28:23 -0300

    CI - Script install-tf-and-aws-cli

---
+++ install-tf-and-aws-cli.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# aws cli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
+
+# terraform
+wget https://releases.hashicorp.com/terraform/"$tf_version"/terraform_"$tf_version"_linux_amd64.zip
+unzip terraform_"$tf_version"_linux_amd64.zip
+mv terraform /usr/local/bin/
+rm terraform_"$tf_version"_linux_amd64.zip
